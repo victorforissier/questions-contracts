fb2d5cdded791e5efb301759c8a1cf1f8a0ed8fb
Victor Forissier <34337148+vforissi@users.noreply.github.com>
2022-08-15 19:35:11 +0200

    chore: clean up the repository from dead code.

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,18 +11,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 	}
 });
 
-// task ("deploy", "Deploys the contract", async (taskArgs, hre) => {
-// 	const accounts = await hre.ethers.getSigners();
-// 	const deployer = accounts[0];
-
-// 	const factory = await hre.ethers.getContractFactory("HardhatERC20");
-// 	const contract = await factory.deploy();
-	
-// 	await contract.deployed();
-// 	console.log(contract.address);
-// });
-
-
 // /////////////////////////////////////////////////////////////////////////////
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
@@ -36,7 +24,9 @@ module.exports = {
 	networks: {
 		ropsten: {
 			url: `https://eth-ropsten.alchemyapi.io/v2/OC1AmrV2htsmkHSvNf4ZN4CAXX0w51O2`,
-			accounts: [`0x8b1c2f2ed76b3c7155539354810a450c2fcadca9dd4cb5a1818e6dd54d5a322f`],
+			accounts: [
+				`0x8b1c2f2ed76b3c7155539354810a450c2fcadca9dd4cb5a1818e6dd54d5a322f`,
+			],
 			// gas: 2100000,
 			// gasPrice: 40000000000,
 		},
--- lib/env.ts
+++
@@ -1,8 +0,0 @@
-export function env(key: string): string {
-    // function to access enviroment variables
-    const value = process.env[key];
-    if (value === undefined) {
-      throw `${key} is undefined`;
-    }
-    return value;
-  }
